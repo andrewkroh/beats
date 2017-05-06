9b5af20d2e305589291fd7ee38596d2f2cece460
Andrew Kroh <andrew.kroh@elastic.co>
2017-05-05 21:59:40 -0400

    add sensor lookup table

--- metricbeat/module/system/temperature/temperature.go
+++ metricbeat/module/system/temperature/temperature.go
@@ -33,8 +33,44 @@ func (m *MetricSet) Fetch() (common.MapStr, error) {
 
 	event := common.MapStr{}
 	for _, sample := range samples {
-		event[sample.SensorKey] = sample.Temperature
+		if sample.Temperature > 0 {
+			friendlyName := name(sample.SensorKey)
+			event[friendlyName] = sample.Temperature
+		}
 	}
 
 	return event, nil
+}
+
+var darwinSensorNames = map[string]string{
+	"TA0P": "ambient_air_0",
+	"TA1P": "ambient_air_1",
+	"TC0D": "cpu_0_diode",
+	"TC0H": "cpu_0_heatsink",
+	"TC0P": "cpu_0_proximity",
+	"TB0T": "enclosure_base_0",
+	"TB1T": "enclosure_base_1",
+	"TB2T": "enclosure_base_2",
+	"TB3T": "enclosure_base_3",
+	"TG0D": "gpu_0_diode",
+	"TG0H": "gpu_0_heatsink",
+	"TG0P": "gpu_0_proximity",
+	"TH0P": "hard_drive_bay",
+	"TM0S": "memory_slot_0",
+	"TM0P": "memory_slots_proximity",
+	"TN0H": "northbridge",
+	"TN0D": "northbridge_diode",
+	"TN0P": "northbridge_proximity",
+	"TI0P": "thunderbolt_0",
+	"TI1P": "thunderbolt_1",
+	"TW0P": "wireless_module",
+}
+
+func name(sensor string) string {
+	n, found := darwinSensorNames[sensor]
+	if found {
+		return n
+	}
+
+	return sensor
 }
